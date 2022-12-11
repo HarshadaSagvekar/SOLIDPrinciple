aba359deb824051c34bd82886c91386da4d36cf1
HarshadaSagvekar <harshada.sagvekar@gmail.com>
2022-12-11 14:41:12 +0530

    SRP violation

---
+++ SRP/SRPSolution/SRPViolation/BusinessModel/Invoice.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SRPViolation.BusinessModel
+{
+    internal class Invoice
+    {
+        private double _amount;
+        private double _discountPercent;
+        private string _discription;
+
+        public Invoice(string discription,double amount,double discountPercent)
+        { 
+            _amount= amount;
+            _discountPercent= discountPercent;
+            _discription= discription;
+        }
+
+        public double Discount
+        {
+            get
+            {
+                return _amount * _discountPercent/100;
+            }
+        }
+
+        public double Tax
+        {
+            get
+            {
+                return _amount * .10;
+            }
+        }
+
+        public double Total
+        {
+            get
+            {
+                return _amount - Discount + Tax;
+            }
+        }
+
+        public void GenerateInvoice()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Discription : " + _discription);
+            sb.Append("\nAmount : " + _amount);
+            sb.Append("\nTax : " + Tax);
+            sb.Append("\nDiscount : " + Discount);
+            sb.Append("\nTotal : " + Total);
+
+            Console.WriteLine(sb.ToString());
+            Console.ReadLine();
+        }
+    }
+
+}
---
+++ SRP/SRPSolution/SRPViolation/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SRPViolation.BusinessModel;
+
+Console.WriteLine("Hello, World!");
+
+Invoice invoice = new Invoice(discription: "Food Invoice", amount: 200, discountPercent: 20);
+invoice.GenerateInvoice();
